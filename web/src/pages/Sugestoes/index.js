8dd93ac66a1bdad251500f1d94a2b13a02bd182e
JoacyMS <joacymsilva@gmail.com>
2020-05-12 14:11:46 -0300

    'Envio' de sugestões

--- web/src/pages/Sugestoes/index.js
+++ web/src/pages/Sugestoes/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
+import api from '../../services/api';
+
 export default function Sugestoes () {
+    const [sugestoes, setSugestoes] = useState('');
+
+    async function handleSugestoes (e) {
+        e.preventDefault();
+
+        try {
+            await api.post('/sugestoes', {
+                sugestoes,
+            });
+        } catch (error) {
+            alert("Erro ao enviar as sugestões");
+        }
+
+    }
+
     return (
         <div className="box">
             <h4 className="titulo">
                 Seu feedback é fundamental para a melhoria do "Gasosa!"
             </h4>
 
-            <form action="">
+            <form onSubmit={handleSugestoes}>
                 <textarea
-                    name=""
-                    id=""
+                    value={sugestoes}
+                    onChange={e => setSugestoes(e.target.value)}
                     rows="15"
+                    required
                 >
                 </textarea>
+                
                 <button type="submit">Enviar</button>
             </form>
         </div>
