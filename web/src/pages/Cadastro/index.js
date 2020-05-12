a217bb2e112c61943069a3253491bb65cd647d1b
JoacyMS <joacymsilva@gmail.com>
2020-05-12 13:25:33 -0300

    Login e cadastro

--- web/src/pages/Cadastro/index.js
+++ web/src/pages/Cadastro/index.js
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
 import gasosa from '../../assets/img/gasosa.jpg';
 
+import api from '../../services/api';
+
 export default function Cadastro () {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+    async function handleCadastro (e) {
+        e.preventDefault();
+
+        if (passwordConfirmation !== password) {
+            alert("Senhas não conferem");
+            return;
+        }
+
+        try {
+            await api.post('/cadastro', {
+                name,
+                email,
+                password,
+            });
+        } catch (error) {
+            alert('Erro no cadastro');
+        }
+    }
+
     return (
         <div className="box">
             <div className="logoCadastro">
                 <img src={gasosa} alt="" />
             </div>
-            
-            <form action={() => { }}>
-                <input type="text" name="name" id="name" placeholder="Nome" required />
-                <input type="email" name="email" id="email" placeholder="Email" required />
-                <input type="password" name="password" id="password" placeholder="Senha" required />
-                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme sua Senha" required />
+
+            <form onSubmit={handleCadastro}>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder="Nome"
+                    required
+                />
+
+                <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                />
+
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Senha"
+                    required
+                />
+
+                <input
+                    type="password"
+                    value={passwordConfirmation}
+                    onChange={e => setPasswordConfirmation(e.target.value)}
+                    placeholder="Confirme sua Senha"
+                    required
+                />
+
                 <button type="submit">Cadastrar</button>
 
                 <Link to="/">
--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
 import gasosa from '../../assets/img/gasosa.jpg';
 
+import api from '../../services/api';
+
 export default function Login () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleLogin (e) {
+        e.preventDefault();
+
+        try {
+            await api.post('/login', {
+                email,
+                password,
+            });
+        } catch (error) {
+            alert('Erro no login');
+        }
+    }
+
     return (
         <div className="box">
             <div className="logo">
                 <img src={gasosa} alt="" />
             </div>
-            
-            <form action={() => { }}>
-                <input type="email" name="email" id="email" placeholder="Email" required />
-                <input type="password" name="password" id="password" placeholder="Senha" required />
+
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                />
+
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Senha"
+                    required
+                />
+
                 <button type="submit">Entrar</button>
 
                 <Link to="/registro">
