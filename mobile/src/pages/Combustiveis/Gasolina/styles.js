58c21855a4400810d465d5557fe95862c7d42fda
Joacy <joacymsilva@gmail.com>
2020-06-10 15:01:08 -0300

    Atualização da página dos combustíveis

--- mobile/src/pages/Combustiveis/Gasolina/styles.js
+++ mobile/src/pages/Combustiveis/Gasolina/styles.js
@@ -5,4 +5,47 @@ export default StyleSheet.create({
         flex: 1,
         backgroundColor: "#51397f",
     },
+
+    list:{    
+        paddingHorizontal: 10,
+        paddingTop: 25,
+    },
+
+    combustivelContainer: {
+        backgroundColor: "#f9f9f9",
+        marginBottom: 20,
+        borderRadius: 4,
+    },
+
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "#7b287d",
+        padding: 10,
+        borderRadius: 4,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+
+    bandeira: {
+        width: 24,
+        height: 24,
+        borderRadius: 50,
+    },
+
+    nome: {
+        color: "#f9f9f9",
+        fontSize: 14,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+
+    body: {
+        backgroundColor: "#f9f9f9",
+        padding: 10,
+        borderRadius: 4,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+    },
 });
